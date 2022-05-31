a856513dd9d835d638cec9761494ffe37dc7b86a
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2022-05-31 12:22:13 +0200

    Added files for github action

---
+++ upload-artifact.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -u
+
+VERSION=$(grep "^version" ./metadata.txt | cut -d= -f2)
+
+# ARTIFACTS_KEY should be set as env variable in the travis UI.
+ARTIFACT=ThreeDiCustomizations.${VERSION}.zip
+PROJECT=ThreeDiCustomizations
+
+curl -X POST \
+     --retry 3 \
+     -H "Content-Type: multipart/form-data" \
+     -F key=${ARTIFACTS_KEY} \
+     -F artifact=@${ARTIFACT} \
+     https://artifacts.lizard.net/upload/${PROJECT}/
